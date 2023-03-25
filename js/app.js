152f9e5ac69e1e3acacbfc044ac817ef4063b386
Design <73782502+Design-Git@users.noreply.github.com>
2023-03-25 20:39:10 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 // variables
+const openMenu = document.querySelector('.open');
+const closeMenu = document.querySelector('.close');
+const menu = document.querySelector('.menu-list');
+
+openMenu.addEventListener('click', ()=> {
+    menu.style.transform = 'translateX(0)'
+})
+closeMenu.addEventListener('click', ()=> {
+    menu.style.transform = 'translateX(100%)'
+})
+
